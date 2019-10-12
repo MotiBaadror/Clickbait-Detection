a941e25e4e551b88e28d42aef98ef27e8bff3f06
MBaadror <motibaadror@gmail.com>
2019-10-12 10:45:10 +0530

    adding Files

---
+++ myimports.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# %load_ext autoreload
+# %autoreload 2
+# %matplotlib inline
+pd.set_option('display.Max_Rows',None,'display.Max_columns',None)
+pd.set_option('display.max_colwidth',6000)
+from IPython.core.display import display, HTML
+display(HTML('<style>.container {width:100%;}</style>'))
+print( """ Imported Modules:
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+pd.set_option('display.Max_Rows',None,'display.Max_columns',None)
+'display.max_colwidth':6000
+from IPython.core.display import display, HTML
+display(HTML('<style>.container {width:100%;}</style>'))""")
